16d9eff6b0f58ba44d1b8f6a3437f7d949beaf09
Nitish Kumar <nitishkumargenius@gmail.com>
2020-11-29 00:10:01 +0530

    Tested arrayreverse again

--- Arrays/reversearray.cpp
+++ Arrays/reversearray.cpp
@@ -3,32 +3,35 @@ Input  : arr[] = {1, 2, 3, 4, 5, 6}
 Output : arr[] = {6, 5, 4, 3, 2, 1}
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void reverseArray(int arr[], int n){
+void reverseArray(int arr[], int n)
+{
 
-    for(int i=0; i<n/2; i++){
+    for (int i = 0; i < n / 2; i++)
+    {
         int temp = arr[i];
-        arr[i] = arr[n-i-1];
-        arr[n-i-1] = temp;
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
     }
 
     //print the array
-    for(int i=0; i<n; i++)
-        cout<<arr[i]<<" ";
-    cout<<endl;
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
     int arr[n];
 
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
     reverseArray(arr, n);
