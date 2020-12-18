c0b095a1c587ee84f3d6e2edf1dc811799190b14
Nitish Kumar <nitishkumargenius@gmail.com>
2020-12-18 14:34:45 +0530

    Added reverse bits

---
+++ BitManipulation/reversebits.cpp
@@ -0,0 +1,22 @@
+/*
+Leetcode Question 190. Reverse Bits
+https://leetcode.com/problems/reverse-bits/
+*/
+
+class Solution
+{
+public:
+    uint32_t reverseBits(uint32_t n)
+    {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            ans = ans << 1;
+            ans = ans | (n & 1);
+            n = n >> 1;
+        }
+
+        return ans;
+    }
+};
