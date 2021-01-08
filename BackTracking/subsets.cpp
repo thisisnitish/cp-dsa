18916d9f8d6cc3bfd1f1d2c414a0aa2066066a10
Nitish Kumar <nitishkumargenius@gmail.com>
2021-01-09 03:16:43 +0530

    Modified subsets

--- BackTracking/subsets.cpp
+++ BackTracking/subsets.cpp
@@ -51,6 +51,31 @@ public:
 };
 
 //Solution 2
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<vector<int>> subset;
+        vector<int> current; //to save the small lists
+        findSubset(subset, 0, nums, current);
+        return subset;
+    }
+
+    void findSubset(vector<vector<int>> &subset, int index, vector<int> &nums, vector<int> &current)
+    {
+        //adding the empty list first of all
+        subset.push_back(current);
+        for (int i = index; i < nums.size(); i++)
+        {
+            current.push_back(nums[i]);
+            findSubset(subset, i + 1, nums, current);
+            current.pop_back();
+        }
+    }
+};
+
+//Solution 3
 //Bit Manipulation
 
 class Solution
