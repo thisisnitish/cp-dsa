36842c70de93687bbaa013434d2e0443a27fdf6f
Nitish Kumar <nitishkumargenius@gmail.com>
2021-01-12 19:45:21 +0530

    Added reverse linked list

---
+++ LinkedList/reverselinkedlist.cpp
@@ -0,0 +1,38 @@
+/*
+Leetcode Question 206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/
+*/
+
+//Iterative
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prevptr, *nextptr, *curr;
+        curr = head;
+        prevptr = nextptr = NULL;
+        while (curr)
+        {
+            nextptr = curr->next;
+            curr->next = prevptr;
+            prevptr = curr;
+            curr = nextptr;
+        }
+        //curr = prevptr;
+        return prevptr;
+    }
+};
+
+//Recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return newhead;
+    }
+};
