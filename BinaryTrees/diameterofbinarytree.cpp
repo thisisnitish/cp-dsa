27a8c60b4d52e581e67c8007ab9183145a5f5f4f
Nitish Kumar <nitishkumargenius@gmail.com>
2021-05-16 17:54:50 +0530

    Update: updated Diameter of Binary Tree

--- BinaryTrees/diameterofbinarytree.cpp
+++ BinaryTrees/diameterofbinarytree.cpp
@@ -13,14 +13,16 @@ https://leetcode.com/problems/diameter-of-binary-tree/
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+*/
+
+//Solution 1
 class Solution
 {
 public:
     /*the basic idea is to find the height of the subtrees
     from left and right and find diameter from left and right
     subtrees and at last return the maximum of them.
-    Time: O(N)*/
+    Time: O(N), Space: O(N)*/
     int diameterOfBinaryTree(TreeNode *root)
     {
         int height = 0;
@@ -42,5 +44,38 @@ public:
         height = max(leftheight, rightheight) + 1;
 
         return max({leftdiameter, rightdiameter, leftheight + rightheight});
+    }
+};
+
+//Solution 2
+class Solution
+{
+public:
+    /*Time: O(N), Space: O(N)*/
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int dia = INT_MIN;
+        diameter(root, dia);
+        return dia - 1;
+    }
+
+    int diameter(TreeNode *root, int &dia)
+    {
+        if (!root)
+            return 0;
+
+        int left = diameter(root->left, dia);
+        int right = diameter(root->right, dia);
+
+        //calculate the height
+        int temp = max(left, right) + 1;
+
+        //no need of this
+        // int ans = max(temp, 1+left+right);
+        // dia = max(dia, ans);
+
+        dia = max(dia, 1 + left + right);
+
+        return temp;
     }
 };
