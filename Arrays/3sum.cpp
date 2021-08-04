1d993f9fe0f547a32ec9d3a8444c06394f26900e
Nitish Kumar <nitishkumargenius@gmail.com>
2021-08-04 12:48:26 +0530

    Update: udpated 3Sum

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -6,15 +6,17 @@ https://leetcode.com/problems/3sum/
 class Solution
 {
 public:
-    vector<vector<int>> threeSum(vector<int> &nums)
+    //Time: O(nlogn + n^2), Space: O(n)
+    vector<vector<int> > threeSum(vector<int> &nums)
     {
 
-        vector<vector<int>> threesum;
+        vector<vector<int> > threesum;
         int len = nums.size();
 
         sort(nums.begin(), nums.end());
 
-        for (unsigned int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++)
+        {
 
             //to handle the duplicates
             if (i > 0 && nums[i] == nums[i - 1])
@@ -24,15 +26,17 @@ public:
             int right = len - 1;
             int x = nums[i];
 
-            while (left < right){
-                if ((x + nums[left] + nums[right]) == 0){
-                    threesum.push_back(vector<int>{x, nums[left], nums[right]});
+            while (left < right)
+            {
+                if ((x + nums[left] + nums[right]) == 0)
+                {
+                    threesum.push_back({x, nums[left], nums[right]});
 
-                    //to handle the duplicates
+                    //to handle the duplicates for left pointer
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
 
-                    //to handle the duplicates
+                    //to handle the duplicates for right pointer
                     while (left < right && nums[right] == nums[right - 1])
                         right--;
 
