0d5443702a21c83fe9ea6cf3d8eb731351f2654a
Nitish Kumar <nitishkumargenius@gmail.com>
2021-08-21 11:08:54 +0530

    Updated: Permutations

--- BackTracking/permutations.cpp
+++ BackTracking/permutations.cpp
@@ -3,23 +3,56 @@ Leetcode Question 46. Permutations
 https://leetcode.com/problems/permutations/
 */
 
+//Solution 1
 class Solution
 {
 public:
-    vector<vector<int>> permute(vector<int> &nums)
+    //Time: O(N.N!), Space: O(N)
+    vector<vector<int> > permutations;
+    vector<vector<int> > permute(vector<int> &nums)
     {
-        /*Solution 1
-        the idea is every digit in the list will 
-        occur equal number of times in the 
-        begin so try all the possibilites, just remove 
-        one digit from the list put it in the 
-        begining and fix it and then we will left with k-1 
-        digs and recurusively call it and 
-        the n append at the end*/
+        generatePermutations(nums, 0);
+        return permutations;
+    }
+
+    void generatePermutations(vector<int> &nums, int idx)
+    {
+        //if we reach at the end
+        if (idx == nums.size())
+        {
+            permutations.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++)
+        {
+            swap(nums[idx], nums[i]);
+            generatePermutations(nums, idx + 1);
+            swap(nums[idx], nums[i]);
+        }
+    }
+};
+
+/*
+Solution 2
+the idea is every digit in the list will 
+occur equal number of times in the 
+begin so try all the possibilites, just remove 
+one digit from the list put it in the 
+begining and fix it and then we will left with k-1 
+digs and recurusively call it and 
+the n append at the end
+*/
+class Solution
+{
+public:
+    vector<vector<int> > ans;
+    vector<vector<int> > permute(vector<int> &nums)
+    {
+
         if (nums.size() <= 1)
             return {nums};
 
-        vector<vector<int>> result;
+        vector<vector<int> > result;
 
         for (int i = 0; i < nums.size(); i++)
         {
@@ -35,37 +68,5 @@ public:
             }
         }
         return result;
-    }
-};
-
-
-//Solution 2
-class Solution
-{
-public:
-    /*here the basic idea is to swap the values one by one
-    and store it*/
-    vector<vector<int>> ans;
-    vector<vector<int>> permute(vector<int> &nums)
-    {
-        if (nums.size() <= 1)
-            return {nums};
-
-        permutations(nums, 0);
-        return ans;
-    }
-
-    void permutations(vector<int> &nums, int idx)
-    {
-        if (idx == nums.size()){
-            ans.push_back(nums);
-            return;
-        }
-        for (int i = idx; i < nums.size(); i++)
-        {
-            swap(nums[i], nums[idx]);
-            permutations(nums, idx + 1);
-            swap(nums[i], nums[idx]);
-        }
     }
 };
