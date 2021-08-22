933da762c4bf48ade44ed2629a0c38a2d1b0795b
Nitish Kumar <nitishkumargenius@gmail.com>
2021-08-22 16:08:58 +0530

    Added: Combinations

--- BackTracking/combinations.cpp
+++ BackTracking/combinations.cpp
@@ -3,25 +3,53 @@ Leetcode Question 77. Combinations
 https://leetcode.com/problems/combinations/
 */
 
+//Solution 1 - TLE
 class Solution
 {
 public:
-    /*please attempt subset and subset2 
-    questions so that you will grasp easily*/
-    vector<vector<int>> combine(int n, int k)
+    vector<vector<int> > combinations;
+    vector<vector<int> > combine(int n, int k)
     {
-        vector<vector<int>> combinations;
-        vector<int> current; //to save the small lists
-        findCombination(combinations, 1, n, k, current);
+        vector<int> ip;
+        for (int i = 1; i <= n; i++)
+            ip.push_back(i);
+        vector<int> op;
+        findCombination(ip, op, k);
         return combinations;
     }
 
-    void findCombination(vector<vector<int>> &combinations, int index, int n, int k, vector<int> &current)
+    void findCombination(vector<int> ip, vector<int> op, int k)
     {
+        if (ip.size() == 0)
+        {
+            if (op.size() == k)
+                combinations.push_back(op);
+            return;
+        }
+        auto op1 = op;
+        auto op2 = op;
+        op1.push_back(ip[0]);
+        ip.erase(ip.begin() + 0);
+        findCombination(ip, op1, k);
+        findCombination(ip, op2, k);
+    }
+};
 
-        /*we want the subsets of size k,
-        if we have the subsets size is k then 
-        add to the combinations array & return*/
+//Solution 2
+class Solution
+{
+public:
+    //Time: O(n!/k!(n-k)!), Space: O(n!/k!(n-k)!)
+    vector<vector<int> > combinations;
+    vector<vector<int> > combine(int n, int k)
+    {
+        vector<int> current;
+        findCombination(1, n, k, current);
+        return combinations;
+    }
+
+    void findCombination(int index, int n, int k, vector<int> &current)
+    {
         if (current.size() == k)
         {
             combinations.push_back(current);
@@ -29,9 +57,9 @@ public:
         }
 
         for (int i = index; i <= n; i++)
-        { // 1-------n
+        {
             current.push_back(i);
-            findCombination(combinations, i + 1, n, k, current);
+            findCombination(i + 1, n, k, current);
             current.pop_back();
         }
     }
