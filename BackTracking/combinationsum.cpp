5823f54924472b05b4930f83ad761ceb58ef4295
Nitish Kumar <nitishkumargenius@gmail.com>
2021-08-24 13:18:00 +0530

    Update: updated Combination Sum

--- BackTracking/combinationsum.cpp
+++ BackTracking/combinationsum.cpp
@@ -3,39 +3,63 @@ Leetcode Question 39. Combination Sum
 https://leetcode.com/problems/combination-sum/
 */
 
+//Solution 1
 class Solution
 {
 public:
-    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    vector<vector<int> > combinations;
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target)
     {
-        vector<vector<int>> combination;
-        vector<int> current; //to save the small lists
-        findCombination(combination, 0, candidates, current, target);
-        return combination;
+        vector<int> current;
+        helper(candidates, current, target, 0);
+        return combinations;
     }
 
-    void findCombination(vector<vector<int>> &combination, int index, vector<int> &candidates, vector<int> &current, int target)
+    void helper(vector<int> &candidates, vector<int> &current, int target, int idx)
     {
-
-        /* the basic idea is using recursion for every
-         possible combination and subtracting a number over and over
-         if target < 0 return because not any use of it else if target == 0
-         push the subset in set and return*/
         if (target < 0)
             return;
-        else if (target == 0)
+        if (target == 0)
         {
-            combination.push_back(current);
+            combinations.push_back(current);
             return;
         }
 
-        for (int i = index; i < candidates.size(); i++)
+        for (int i = idx; i < candidates.size(); i++)
         {
             current.push_back(candidates[i]);
-            /* In subset problem we didn't used the same number again 
-            but this problem allowing us to do this */
-            findCombination(combination, i, candidates, current, target - candidates[i]);
+            helper(candidates, current, target - candidates[i], i);
             current.pop_back();
         }
+    }
+};
+
+//Solution 2
+class Solution
+{
+public:
+    vector<vector<int> > combinations;
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target)
+    {
+        vector<int> current;
+        helper(candidates, current, target, 0);
+        return combinations;
+    }
+
+    void helper(vector<int> &candidates, vector<int> &op, int target, int idx)
+    {
+        if (target == 0)
+        {
+            combinations.push_back(op);
+            return;
+        }
+        if (idx == candidates.size() || target < 0)
+            return;
+
+        auto op1 = op;
+        auto op2 = op;
+        op1.push_back(candidates[idx]);
+        helper(candidates, op1, target - candidates[idx], idx);
+        helper(candidates, op2, target, idx + 1);
     }
 };
