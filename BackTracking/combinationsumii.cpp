655d438d5f929278b38a8ff8caa8312b43bec5c7
Nitish Kumar <nitishkumargenius@gmail.com>
2021-08-25 12:48:36 +0530

    Added: Combination Sum II

--- BackTracking/combinationsumii.cpp
+++ BackTracking/combinationsumii.cpp
@@ -3,44 +3,72 @@ Leetcode Question 40. Combination Sum II
 https://leetcode.com/problems/combination-sum-ii/
 */
 
+//Solution 1
 class Solution
 {
 public:
-    /*what i feel is this question is a combination of 
-    subset, subset-ii and combination sum*/
-    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    //Time: O(2^N), Space: O(N)
+    vector<vector<int> > combinations;
+    vector<vector<int> > combinationSum2(vector<int> &candidates, int target)
     {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> combination;
         vector<int> current;
-
-        findCombination(combination, 0, candidates, current, target);
-        return combination;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, current, target, 0);
+        return combinations;
     }
 
-    void findCombination(vector<vector<int>> &combination, int index, vector<int> &candidates, vector<int> &current, int target)
+    void helper(vector<int> &candidates, vector<int> &current, int target, int idx)
     {
-
-        /* same as combination sum but here we don't
-        have to use the same number again and again*/
         if (target < 0)
             return;
         if (target == 0)
         {
-            combination.push_back(current);
+            combinations.push_back(current);
             return;
         }
-
-        for (int i = index; i < candidates.size(); i++)
+        for (int i = idx; i < candidates.size(); i++)
         {
-
-            //handling duplicates
-            if (i > index && candidates[i] == candidates[i - 1])
+            if (i > idx && candidates[i] == candidates[i - 1])
                 continue;
-
             current.push_back(candidates[i]);
-            findCombination(combination, i + 1, candidates, current, target - candidates[i]);
+            helper(candidates, current, target - candidates[i], i + 1);
             current.pop_back();
         }
+    }
+};
+
+//Solution 2
+//This will give you TLE
+class Solution
+{
+public:
+    vector<vector<int> > combinations;
+    vector<vector<int> > combinationSum2(vector<int> &candidates, int target)
+    {
+        vector<int> current;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, current, target, 0);
+        return combinations;
+    }
+
+    void helper(vector<int> &candidates, vector<int> &current, int target, int idx)
+    {
+        if (target < 0)
+            return;
+        if (target == 0)
+        {
+            if (find(combinations.begin(), combinations.end(), current) == combinations.end())
+            {
+                combinations.push_back(current);
+            }
+            return;
+        }
+        if (idx == candidates.size())
+            return;
+        auto op1 = current;
+        auto op2 = current;
+        op1.push_back(candidates[idx]);
+        helper(candidates, op1, target - candidates[idx], idx + 1);
+        helper(candidates, op2, target, idx + 1);
     }
 };
