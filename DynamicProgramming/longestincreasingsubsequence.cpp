ac14881e89efd2bd9379e042157bfd40ea8c6c76
Nitish Kumar <nitishkumargenius@gmail.com>
2021-09-21 20:04:50 +0530

    MUpdated: Longest Increasing subsequence

--- DynamicProgramming/longestincreasingsubsequence.cpp
+++ DynamicProgramming/longestincreasingsubsequence.cpp
@@ -97,21 +97,19 @@ public:
     {
         int n = nums.size();
         vector<int> dp(n, 1);
-        int result = 0;
+        int result = 1;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < i; j++)
             {
                 if (nums[i] > nums[j])
+                {
                     dp[i] = max(dp[i], dp[j] + 1);
+                }
+                result = max(result, dp[i]);
             }
         }
-
-        //get the maximum value
-        for (int i = 0; i < dp.size(); i++)
-            result = max(result, dp[i]);
-
         return result;
     }
 };
