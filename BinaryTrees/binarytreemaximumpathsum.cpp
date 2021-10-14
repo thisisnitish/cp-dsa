ed78513e168cea4b53124ae7a0086419c449dfad
Nitish Kumar <nitishkumargenius@gmail.com>
2021-10-14 14:25:00 +0530

    Update: Binary Tree Maximum Path Sum

--- BinaryTrees/binarytreemaximumpathsum.cpp
+++ BinaryTrees/binarytreemaximumpathsum.cpp
@@ -33,8 +33,14 @@ public:
         int left = helper(root->left, maxPath);
         int right = helper(root->right, maxPath);
 
+        //find the maximum sum through the root node
         int temp = max(max(left, right) + root->val, root->val);
+
+        //find the maximum sum through the path irrespective of the root node
         int ans = max(temp, left + right + root->val);
+
+        //since we will be having the maximum sum stored in ans and now we only have to
+        //compare with our final answer
         maxPath = max(maxPath, ans);
         return temp;
     }
