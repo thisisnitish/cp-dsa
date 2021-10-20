103d242c464ca9af0aa58e6ebc4cd726bf505ed0
Nitish Kumar <nitishkumargenius@gmail.com>
2021-10-20 12:04:51 +0530

    Updated: Single Number II

--- BitManipulation/singlenumberii.cpp
+++ BitManipulation/singlenumberii.cpp
@@ -3,43 +3,73 @@ Leetcode Question 137. Single Number II
 https://leetcode.com/problems/single-number-ii/
 */
 
+//Solution 1
 class Solution
 {
 public:
- 
-    int getBit(int n, int pos)
-    {
-        return ((n & (1 << pos)) != 0);
-    }
-
-    int setBit(int n, int pos)
-    {
-        return (n | (1 << pos));
-    }
-
+    //Time: O(N), Space: O(N)
     int singleNumber(vector<int> &nums)
     {
-        int result = 0;
-
-        //traversing from 0 to 32 in order to cover all the bits
-        // can also take 64 in place of 32
-        for (int i = 0; i < 32; i++)
+        unordered_map<int, int> mp;
+        for (auto n : nums)
         {
-            int sum = 0;
-            //traverse all the elements and checking the set bit at ith position in every element
-            for (int j = 0; j < nums.size(); j++)
-            {
-                if (getBit(nums[j], i))
-                {
-                    sum++;
-                }
-            }
-
-            if (sum % 3 != 0)
-            {
-                result = setBit(result, i);
-            }
+            mp[n]++;
         }
-        return result;
+
+        for (auto [k, v] : mp)
+        {
+            if (v == 1)
+                return k;
+        }
+        return -1;
+    }
+};
+
+//Solution 2
+class Solution
+{
+public:
+    //Time: O(NlogN), Space: O(N)
+    int singleNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        sort(nums.begin(), nums.end());
+
+        //base conditions
+        //desired element could be at first position or last position
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        for (int i = 1; i < n - 1; i++)
+        {
+            //if the condition is true then nums[i] will be the unique number
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+                return nums[i];
+        }
+        return -1;
+    }
+};
+
+//Solution 3
+class Solution
+{
+public:
+    //Time: O(N), Space: O(1)
+    int singleNumber(vector<int> &nums)
+    {
+        /*ones store the number occurs only one time and twos 
+        stores the number occurs only two times*/
+        int ones = 0, twos = 0;
+
+        for (auto n : nums)
+        {
+            ones = (ones ^ n) & (~twos);
+            twos = (twos ^ n) & (~ones);
+        }
+        return ones;
     }
 };
