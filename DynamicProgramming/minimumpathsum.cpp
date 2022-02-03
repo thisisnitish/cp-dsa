884cc45a4896fea7b9ec54fb24369153f1d5f005
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-03 14:06:11 +0530

    updated: Minimum Path Sum

--- DynamicProgramming/minimumpathsum.cpp
+++ DynamicProgramming/minimumpathsum.cpp
@@ -3,66 +3,92 @@ Leetcode Question 64. Minimum Path Sum
 https://leetcode.com/problems/minimum-path-sum/
 */
 
-//Recursive + Memoization, BottomUp
+// Recursive
 class Solution
 {
 public:
-    //Time: O(m*n), Space: O(m*n)
+    // Time: O(2^m*n), Space: O(2^m*n)
     int minPathSum(vector<vector<int> > &grid)
     {
-        int m = grid.size();    //rows
-        int n = grid[0].size(); //cols;
-
-        vector<vector<int> > dp(m, vector<int>(n, -1));
-        return helper(grid, m, n, dp, 0, 0);
+        int m = grid.size();    // rows
+        int n = grid[0].size(); // cols;
+        return helper(grid, m, n, 0, 0);
     }
 
-    int helper(vector<vector<int> > &grid, int m, int n, vector<vector<int> > &dp, int x, int y)
+    int helper(vector<vector<int> > &grid, int m, int n, int x, int y)
     {
-        //Base condition
+        // Base condition
         if (x == m - 1 && y == n - 1)
             return grid[x][y];
         if (x >= m || y >= n)
             return INT_MAX;
-        if (dp[x][y] != -1)
-            return dp[x][y];
 
-        //recursive calls and checking for the minimum path
-        dp[x][y] = grid[x][y] + min(
-                                    helper(grid, m, n, dp, x, y + 1),
-                                    helper(grid, m, n, dp, x + 1, y));
-        return dp[x][y];
+        // recursive calls and checking for the minimum path
+        return grid[x][y] +
+               min(helper(grid, m, n, x, y + 1), helper(grid, m, n, x + 1, y));
     }
 };
 
-//TopDown
+// Recursive + Memoization, BottomUp
 class Solution
 {
 public:
-    //Time: O(m*n), Space: O(m*n)
+    // Time: O(m*n), Space: O(m*n)
+    vector<vector<int> > memo;
     int minPathSum(vector<vector<int> > &grid)
     {
-        int m = grid.size(); //rows
+        int m = grid.size();    // rows
+        int n = grid[0].size(); // cols;
+
+        memo.resize(m, vector<int>(n, -1));
+        return helper(grid, m, n, 0, 0);
+    }
+
+    int helper(vector<vector<int> > &grid, int m, int n, int x, int y)
+    {
+        // Base condition
+        if (x == m - 1 && y == n - 1)
+            return grid[x][y];
+        if (x >= m || y >= n)
+            return INT_MAX;
+        if (memo[x][y] != -1)
+            return memo[x][y];
+
+        // recursive calls and checking for the minimum path
+        memo[x][y] = grid[x][y] +
+                     min(helper(grid, m, n, x, y + 1), helper(grid, m, n, x + 1, y));
+        return memo[x][y];
+    }
+};
+
+// TopDown
+class Solution
+{
+public:
+    // Time: O(m*n), Space: O(m*n)
+    int minPathSum(vector<vector<int> > &grid)
+    {
+        int m = grid.size(); // rows
 
         if (m == 0)
             return 0;
 
-        int n = grid[0].size(); //cols;
+        int n = grid[0].size(); // cols;
 
         vector<vector<int> > dp(m, vector<int>(n, 0));
 
-        //fill the first value
+        // fill the first value
         dp[0][0] = grid[0][0];
 
-        //initializing the row
-        for (int i = 1; i < n; i++)
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        // initializing the first row
+        for (int j = 1; j < n; j++)
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
 
-        //initializing the column
+        // initializing the first column
         for (int i = 1; i < m; i++)
             dp[i][0] = dp[i - 1][0] + grid[i][0];
 
-        //solving the subproblems
+        // solving the subproblems
         for (int i = 1; i < m; i++)
         {
             for (int j = 1; j < n; j++)
@@ -74,34 +100,32 @@ public:
     }
 };
 
-//TopDown Space Optimze
+// TopDown Space Optimze
 class Solution
 {
 public:
-    /*Time: O(m*n), Space: O(1)*/
+    // Time: O(m*n), Space: O(1)
     int minPathSum(vector<vector<int> > &grid)
     {
-        int m = grid.size(); //rows
+        int m = grid.size(); // rows
 
         if (m == 0)
             return 0;
 
-        int n = grid[0].size(); //cols;
+        int n = grid[0].size(); // cols;
 
-        // vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        //fill the first value
+        // fill the first value
         grid[0][0] = grid[0][0];
 
-        //initializing the row
-        for (int i = 1; i < n; i++)
-            grid[0][i] = grid[0][i - 1] + grid[0][i];
+        // initializing the first row
+        for (int j = 1; j < n; j++)
+            grid[0][j] = grid[0][j - 1] + grid[0][j];
 
-        //initializing the column
+        // initializing the first column
         for (int i = 1; i < m; i++)
             grid[i][0] = grid[i - 1][0] + grid[i][0];
 
-        //solving the subproblems
+        // solving the subproblems
         for (int i = 1; i < m; i++)
         {
             for (int j = 1; j < n; j++)
