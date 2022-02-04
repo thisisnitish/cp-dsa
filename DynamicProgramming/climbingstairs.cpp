53a4c946bf67ce8913dfbcf18e038a7fd05df23f
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-04 13:42:22 +0530

    updated: Climbing Stairs

--- DynamicProgramming/climbingstairs.cpp
+++ DynamicProgramming/climbingstairs.cpp
@@ -3,6 +3,68 @@ Leetcode Question 70. Climbing Stairs
 https://leetcode.com/problems/climbing-stairs/
 */
 
+// Recursive
+class Solution
+{
+public:
+    // Time: O(2^n), Space: O(2^n)
+    int climbStairs(int n)
+    {
+        // base case
+        if (n <= 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
+
+// Recursive + Memoization
+class Solution
+{
+public:
+    // Time: O(n), Space: O(n)
+    vector<int> memo;
+    int countSteps(int n)
+    {
+        // base case
+        if (n <= 0)
+            return memo[n] = 0;
+        if (n == 1 || n == 2)
+            return memo[n] = n;
+        if (memo[n] != -1)
+            return memo[n];
+
+        return memo[n] = countSteps(n - 1) + countSteps(n - 2);
+    }
+    int climbStairs(int n)
+    {
+        memo.resize(n + 1, -1);
+        return countSteps(n);
+    }
+};
+
+// Tabulation
+class Solution
+{
+public:
+    // Time: O(n), Space: O(n)
+    int climbStairs(int n)
+    {
+        if (n <= 1)
+            return n;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
+    }
+};
+
 /*This is a variation of Fibonacci*/
 // Recursive
 class Solution
@@ -41,28 +103,7 @@ public:
     }
 };
 
-//Tabulation
-class Solution
-{
-public:
-    // Time: O(n), Space: O(n)
-    int climbStairs(int n)
-    {
-        if (n <= 1)
-            return n;
-        vector<int> dp(n + 1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for (int i = 3; i < n + 1; i++)
-            dp[i] = dp[i - 1] + dp[i - 2];
-
-        return dp[n];
-    }
-};
-
-// Fibonacci
+// Tabulation
 class Solution
 {
 public:
