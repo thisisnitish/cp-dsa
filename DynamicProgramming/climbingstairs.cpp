8830261afe6c81721343daa4e3ae9f89da9c5def
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-04 13:13:39 +0530

    updated: Climbing Stairs

--- DynamicProgramming/climbingstairs.cpp
+++ DynamicProgramming/climbingstairs.cpp
@@ -41,7 +41,28 @@ public:
     }
 };
 
-// Tabulation
+//Tabulation
+class Solution
+{
+public:
+    // Time: O(n), Space: O(n)
+    int climbStairs(int n)
+    {
+        if (n <= 1)
+            return n;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < n + 1; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
+    }
+};
+
+// Fibonacci
 class Solution
 {
 public:
