24ec4fa2d17389b884bcc05ea7ac88619e8ccd25
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-07 22:50:22 +0530

    Added Min Cost Climbing Stairs

--- DynamicProgramming/minimumcostclimbingstairs.cpp
+++ DynamicProgramming/minimumcostclimbingstairs.cpp
@@ -3,66 +3,92 @@ Leetcode Question 746. Min Cost Climbing Stairs
 https://leetcode.com/problems/min-cost-climbing-stairs/
 */
 
-//Recursive
+/*
+This is a variation for LC: 70
+*/
+// Recursive
 class Solution
 {
 public:
-    int minCostClimbingStairs(vector<int> &cost)
+    // Time: O(2^N), Space: O(2^N)
+    int findMinCost(vector<int> &cost, int idx)
     {
-        int idx = cost.size();
-        return min(helper(cost, idx - 1), helper(cost, idx - 2));
+        // base case
+        if (idx >= cost.size())
+            return 0;
+
+        return cost[idx] + min(findMinCost(cost, idx + 1), findMinCost(cost, idx + 2));
     }
 
-    int helper(vector<int> &cost, int idx)
+    int minCostClimbingStairs(vector<int> &cost)
     {
-        if (idx == 0 || idx == 1)
-            return cost[idx];
-
-        return min(helper(cost, idx - 1), helper(cost, idx - 2)) + cost[idx];
+        /*Since you can start from idx 0 or 1. You have to explore both the
+        paths and get the min result*/
+        return min(findMinCost(cost, 0), findMinCost(cost, 1));
     }
 };
 
-//Recursive + Memoization, Top Down
+// Recursive + Memoization
 class Solution
 {
 public:
-    int minCostClimbingStairs(vector<int> &cost)
+    // Time: O(N), Space: O(N)
+    int findMinCost(vector<int> &cost, int idx, vector<int> &memo)
     {
-        int idx = cost.size();
-
-        //not idx+1 because first block will be empty so don't do it
-        vector<int> memo(idx, -1);
-        return min(helper(cost, idx - 1, memo), helper(cost, idx - 2, memo));
-    }
-
-    int helper(vector<int> &cost, int idx, vector<int> &memo)
-    {
-        if (idx == 0 || idx == 1)
-            return cost[idx];
+        // base case
+        if (idx >= cost.size())
+            return 0;
 
         if (memo[idx] != -1)
             return memo[idx];
 
-        memo[idx] = min(helper(cost, idx - 1, memo), helper(cost, idx - 2, memo)) + cost[idx];
-        return memo[idx];
+        return memo[idx] = cost[idx] +
+                           min(findMinCost(cost, idx + 1, memo), findMinCost(cost, idx + 2, memo));
+    }
+
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        vector<int> memo(cost.size(), -1);
+        return min(findMinCost(cost, 0, memo), findMinCost(cost, 1, memo));
     }
 };
 
-//Bottom Up
+// Tabulation
 class Solution
 {
 public:
+    // Time: O(N), Space: O(N)
     int minCostClimbingStairs(vector<int> &cost)
     {
-        int idx = cost.size();
-        vector<int> dp(idx, 0);
+        // dp[i] is defined as the min cost to reach ith step
+        vector<int> dp(cost.size(), 0);
         dp[0] = cost[0];
         dp[1] = cost[1];
 
-        for (int i = 2; i < idx; i++)
+        for (int i = 2; i < cost.size(); i++)
         {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
         }
-        return min(dp[idx - 1], dp[idx - 2]);
+        return min(dp[cost.size() - 1], dp[cost.size() - 2]);
+    }
+};
+
+// Space Optimsed
+class Solution
+{
+public:
+    // Time: O(1)
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int firstStep = cost[0], secondStep = cost[1];
+        int nextStep;
+
+        for (int i = 2; i < cost.size(); i++)
+        {
+            nextStep = cost[i] + min(firstStep, secondStep);
+            firstStep = secondStep;
+            secondStep = nextStep;
+        }
+        return min(firstStep, secondStep);
     }
 };
