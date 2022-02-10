efeae3948726bf0f5a18ca26b48b35d6df42a769
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-10 13:37:44 +0530

    updated Maximum Length of Repeated Subarray

--- DynamicProgramming/maximumlengthofrepeatedsubarray.cpp
+++ DynamicProgramming/maximumlengthofrepeatedsubarray.cpp
@@ -16,7 +16,7 @@ knowing what I'm talking about. Technically, we have to find the Longest Common 
 class Solution
 {
 public:
-    //Time: O(2^(n+m)), Space: O(n)
+    // Time: O(2^(n+m))
     int findLength(vector<int> &nums1, vector<int> &nums2)
     {
         int m = nums1.size();
@@ -30,51 +30,22 @@ public:
             return result;
 
         if (nums1[m - 1] == nums2[n - 1])
-            return result = helper(nums1, nums2, m - 1, n - 1, result + 1);
-        return max({result, helper(nums1, nums2, m, n - 1, 0), helper(nums1, nums2, m - 1, n, 0)});
-    }
-};
+        {
+            result = helper(nums1, nums2, m - 1, n - 1, result + 1);
+        }
 
-//Recursive + Memoization
-class Solution
-{
-public:
-    //Time: O(m*n), Space: O(m*n), without including the recursion stack
-    int result = 0;
-    int findLength(vector<int> &nums1, vector<int> &nums2)
-    {
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<vector<int> > memo(m + 1, vector<int>(n + 1, -1));
-        helper(nums1, nums2, m, n, memo);
+        result = max({result,
+                      helper(nums1, nums2, m, n - 1, 0),
+                      helper(nums1, nums2, m - 1, n, 0)});
         return result;
     }
-
-    int helper(vector<int> &nums1, vector<int> &nums2, int m, int n, vector<vector<int> > &memo)
-    {
-        if (m == 0 || n == 0)
-            return 0;
-
-        if (memo[m][n] != -1)
-            return memo[m][n];
-
-        helper(nums1, nums2, m, n - 1, memo);
-        helper(nums1, nums2, m - 1, n, memo);
-
-        if (nums1[m - 1] == nums2[n - 1])
-        {
-            memo[m][n] = 1 + helper(nums1, nums2, m - 1, n - 1, memo);
-            result = max(result, memo[m][n]);
-            return memo[m][n];
-        }
-        return memo[m][n] = 0;
-    }
 };
 
+// Tabulation
 class Solution
 {
 public:
-    //Time: O(m*n), Space: O(m*n), without including the recursion stack
+    // Time: O(m*n), Space: O(m*n)
     int findLength(vector<int> &nums1, vector<int> &nums2)
     {
         int m = nums1.size();
@@ -82,7 +53,7 @@ public:
         int result = 0;
         vector<vector<int> > dp(m + 1, vector<int>(n + 1));
 
-        //Initialization
+        // Initialization
         for (int i = 0; i < m + 1; i++)
         {
             for (int j = 0; j < n + 1; j++)
@@ -92,7 +63,7 @@ public:
             }
         }
 
-        //solving the subproblems
+        // solving the subproblems
         for (int i = 1; i < m + 1; i++)
         {
             for (int j = 1; j < n + 1; j++)
@@ -100,10 +71,10 @@ public:
                 if (nums1[i - 1] == nums2[j - 1])
                 {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                    result = max(result, dp[i][j]);
                 }
                 else
                     dp[i][j] = 0;
+                result = max(result, dp[i][j]);
             }
         }
         return result;
