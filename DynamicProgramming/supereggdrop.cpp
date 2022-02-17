0524d7df732aa43663c1dcbe255600a8df078ae4
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-17 13:42:16 +0530

    updated Super Egg Drop

--- DynamicProgramming/supereggdrop.cpp
+++ DynamicProgramming/supereggdrop.cpp
@@ -25,11 +25,12 @@ Base Conditions:
 3. If the n is 0 then no floor 0 attempt.
 4. If the n is 1 then only 1 attempt.
 */
-//Recursive
+
+// Recursive
 class Solution
 {
 public:
-    //Time: O(n*(2^min(n,k))), Space: O(n)
+    // Time: O(n*(2^min(n,k))), Space: O(n)
     int helper(int k, int n)
     {
         if (n == 0 || n == 1)
@@ -42,30 +43,32 @@ public:
         for (int i = 1; i <= n; i++)
         {
 
-            /*representing both the choices
-            First one, if the egg will break, no. of eggs will decreased and we have to
-            down from that floor.
-            Second one, if the egg will not break, no. of eggs will not decreased and we
-            have to go above form that floor.*/
+            /*
+            representing both the choices
+            First one, if the egg will break, no. of eggs will decreased and we have
+            to down from that floor.
+            Second one, if the egg will not break, no. of eggs will not decreased and
+            we have to go above form that floor.
+            */
             int temp = 1 + max(helper(k - 1, i - 1), helper(k, n - i));
 
-            mn = min(mn, temp); //minimum number of attempts
+            mn = min(mn, temp); // minimum number of attempts
         }
         return mn;
     }
 
     int superEggDrop(int k, int n)
     {
-        //k means number of eggs, n means number of floors
+        // k means number of eggs, n means number of floors
         return helper(k, n);
     }
 };
 
-//Recursive + Memoized TopDown
+// Recursive + Memoized TopDown
 class Solution
 {
 public:
-    //Time: O(n^2*k), Space: O(n*k)
+    // Time: O(n^2*k), Space: O(n*k)
     int helper(int k, int n, vector<vector<int> > &memo)
     {
         if (n == 0 || n == 1)
@@ -81,31 +84,33 @@ public:
         for (int i = 1; i <= n; i++)
         {
 
-            /*representing both the choices with memo
-            First one, if the egg will break, no. of eggs will decreased and we have to
-            down from that floor.
-            Second one, if the egg will not break, no. of eggs will not decreased and we
-            have to go above form that floor.*/
+            /*
+            representing both the choices with memo
+            First one, if the egg will break, no. of eggs will decreased and we have
+            to down from that floor.
+            Second one, if the egg will not break, no. of eggs will not decreased and
+            we have to go above form that floor.
+            */
             int temp = 1 + max(helper(k - 1, i - 1, memo), helper(k, n - i, memo));
 
-            mn = min(mn, temp); //minimum number of attempts
+            mn = min(mn, temp); // minimum number of attempts
         }
         return memo[k][n] = mn;
     }
 
     int superEggDrop(int k, int n)
     {
-        //k means number of eggs, n means number of floors
+        // k means number of eggs, n means number of floors
         vector<vector<int> > memo(k + 1, vector<int>(n + 1, -1));
         return helper(k, n, memo);
     }
 };
 
-//Optimized Memoization
+// Optimized Memoization
 class Solution
 {
 public:
-    //Time: O(n^2*k), Space: O(n*k)
+    // Time: O(n^2*k), Space: O(n*k)
     int helper(int k, int n, vector<vector<int> > &memo)
     {
         if (n == 0 || n == 1)
@@ -121,9 +126,11 @@ public:
         for (int i = 1; i <= n; i++)
         {
 
-            /*here is the optimization goes on, the basic idea is before 
-            any function call we are checking if any value from that function 
-            call is present or not*/
+            /*
+            here is the optimization goes on, the basic idea is before
+            any function call we are checking if any value from that function
+            call is present or not
+            */
             int left = 0, right = 0;
             if (memo[k - 1][i - 1] != -1)
                 left = memo[k - 1][i - 1];
@@ -143,24 +150,24 @@ public:
 
             int temp = 1 + max(left, right);
 
-            mn = min(mn, temp); //minimum number of attempts
+            mn = min(mn, temp); // minimum number of attempts
         }
         return memo[k][n] = mn;
     }
 
     int superEggDrop(int k, int n)
     {
-        //k means number of eggs, n means number of floors
+        // k means number of eggs, n means number of floors
         vector<vector<int> > memo(k + 1, vector<int>(n + 1, -1));
         return helper(k, n, memo);
     }
 };
 
-//Memoization + Binary Search
+// Memoization + Binary Search
 class Solution
 {
 public:
-    //Time: O(n*k*logn), Space: O(n*k)
+    // Time: O(n*k*logn), Space: O(n*k)
     int helper(int k, int n, vector<vector<int> > &memo)
     {
         if (n == 0 || n == 1)
@@ -178,31 +185,33 @@ public:
 
             int mid = (low + high) / 2;
 
-            /*representing both the choices with memo
-            First one, if the egg will break, no. of eggs will decreased and we have to
-            down from that floor.
-            Second one, if the egg will not break, no. of eggs will not decreased and we
-            have to go above form that floor.*/
+            /*
+            representing both the choices with memo
+            First one, if the egg will break, no. of eggs will decreased and we have
+            to down from that floor.
+            Second one, if the egg will not break, no. of eggs will not decreased and
+            we have to go above form that floor.
+            */
 
             int left = helper(k - 1, mid - 1, memo);
             int right = helper(k, n - mid, memo);
 
             temp = 1 + max(left, right);
 
-            //since we need more temp value in worst case, so need to go above
+            // since we need more temp value in worst case, so need to go above
             if (left < right)
                 low = mid + 1;
             else
-                high = mid - 1; //move to the downward
+                high = mid - 1; // move to the downward
 
-            mn = min(mn, temp); //minimum number of attempts
+            mn = min(mn, temp); // minimum number of attempts
         }
         return memo[k][n] = mn;
     }
 
     int superEggDrop(int k, int n)
     {
-        //k means number of eggs, n means number of floors
+        // k means number of eggs, n means number of floors
         vector<vector<int> > memo(k + 1, vector<int>(n + 1, -1));
         return helper(k, n, memo);
     }
