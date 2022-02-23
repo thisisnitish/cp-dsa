868fda526ffb9af2e99c7d9afb18ca5062441714
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-23 14:20:13 +0530

    Updated Detect cycle in a directed graph

--- Graph/detectcycleindirectedgraph.cpp
+++ Graph/detectcycleindirectedgraph.cpp
@@ -1,88 +1,49 @@
-/*CPP code for detecting the cycle in directed graph*/
 /*
-Time: O(V+E)
-Space: O(V)
-
-Algorithm using DFS:
-1. Create the graph using the given number of edges and vertices.
-2. Create a recursive function that initializes the current index or vertex, visited, and recursion stack.
-3. Mark the current node as visited and also mark the index in recursion stack.
-4. Find all the vertices which are not visited and are adjacent to the current node. 
-    Recursively call the function for those vertices, If the recursive function returns true, return true.
-5. If the adjacent vertices are already marked in the recursion stack then return true.
-6. Create a wrapper class, that calls the recursive function for all the vertices and 
-   if any function returns true return true. Else if for all vertices the function returns false return false.
+Detect cycle in a directed graph
+https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1/#
 */
 
-//DFS
-#include<iostream>
-#include<vector>
-#include<list>
-using namespace std;
-
-class Graph{
-    int V;
-    list<int> *adj;
-
+class Solution
+{
 public:
-    Graph(int V);
-    void addEdge(int v, int w);
-    bool isCyclic();
-    bool isCyclicUtil(int v, vector<bool> &visited, vector<bool> &recursion_stack);
-};
+    // Function to detect cycle in a directed graph.
+    // we are using DFS here
+    // Time: O(N+E), Space: O(2N) + Stack Space
+    bool checkCycle(int node, vector<int> adj[], vector<int> &vis, vector<int> &dfsVis)
+    {
+        vis[node] = 1;
+        dfsVis[node] = 1;
 
-Graph::Graph(int V){
-    this->V = V;
-    adj = new list<int>[V];
-}
-
-void Graph::addEdge(int v, int w){
-    adj[v].push_back(w);
-}
-
-bool Graph::isCyclicUtil(int v, vector<bool> &visited, vector<bool> &recursive_stack){
-    if(!visited[v]){
-        visited[v] = true;
-        recursive_stack[v] = true;
-
-        /*Recur for all the vertices adjacent to this vertex*/
-        list<int> ::iterator itr;
-        for(itr = adj[v].begin(); itr != adj[v].end(); itr++){
-            if(!visited[*itr] && isCyclicUtil(*itr, visited, recursive_stack)) return true;
-            else if(recursive_stack[*itr]) return true;
+        // go for the adjacent node
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                if (checkCycle(it, adj, vis, dfsVis))
+                    return true;
+            }
+            // if both vis[it] and dfsVis[it] equals 1 then cycle exists
+            else if (dfsVis[it] == 1)
+                return true;
         }
-    }
-    recursive_stack[v] = false;
-    return false;
-}
-
-bool Graph::isCyclic(){
-    vector<bool> visited(V, false);
-    vector<bool> recursion_stack(V, false);
-
-    for(int i=0; i<V; i++){
-        if(isCyclicUtil(i, visited, recursion_stack))
-            return true;
-    }
-    return false;
-}
-
-int main(){
-    int V, E;
-    cout<<"\nEnter the number of vertices: ";
-    cin>>V;
-    cout<<"\nEnter the number of edges: ";
-    cin>>E;
-
-    Graph graph(V);
-
-    int v, w;
-    for(int i=0; i<E; i++){
-        cin>>v>>w;
-        graph.addEdge(v, w);
+        dfsVis[node] = 0; // when we didn't found a cycle
+        return false;
     }
 
-    if(graph.isCyclic()) cout<<"Graph contains cycle";
-    else cout<<"Graph does not contains cycle";
-    return 0;
-}
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        vector<int> vis(V, 0);
+        vector<int> dfsVis(V, 0);
+
+        // graph might be disconnected
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (checkCycle(i, adj, vis, dfsVis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
