106050352b877df2d7d5b4e68520f62f7ccd288c
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-23 12:36:02 +0530

    upadted Is Graph Bipartite?

--- Graph/isgraphbipartite.cpp
+++ Graph/isgraphbipartite.cpp
@@ -3,14 +3,18 @@ Leetcode Question 785. Is Graph Bipartite?
 https://leetcode.com/problems/is-graph-bipartite/
 */
 
-//Using BFS
+/*
+the basic idea is to use graph coloring problem, since we know the
+definition of bipartite graph and graph coloring using 2 colors divides
+the vertices into 2 sets which can tells us whether the graph is a bipartite
+graph or not. Here we are using bfs but we can also use dfs.
+*/
+
+// using BFS
 class Solution
 {
 public:
-    /*the basic idea is to use graph coloring problem, since we know the 
-    definition of bipartite graph and graph coloring using 2 colors divides 
-    the vertices into 2 sets which can tells us whether the graph is a bipartite
-    graph or not. Here we are using bfs but we can also use dfs Time: O(V+E)*/
+    // Time: O(N+E), Space: O(N+E) + O(N) + O(N)
     bool isBipartite(vector<vector<int> > &graph)
     {
         int n = graph.size();
@@ -18,24 +22,24 @@ public:
         //-1 means no vertices are colored
         vector<int> color(n, -1);
 
-        /*this loop is for all vertices because it is given that graph might 
-        be disconnected, so we have to check for every vertices*/
+        /*
+        this loop is for all vertices because it is given that graph might
+        be disconnected, so we have to check for every vertices
+        */
         for (int i = 0; i < n; i++)
         {
             if (color[i] == -1)
-                if (!graphColoring(graph, i, color))
+                if (!checkBipartite(graph, i, color))
                     return false;
         }
         return true;
     }
 
-    bool graphColoring(vector<vector<int> > &graph, int vertex, vector<int> &color)
+    bool checkBipartite(vector<vector<int> > &graph, int vertex, vector<int> &color)
     {
-        color[vertex] = 1;
-
         queue<int> q;
-
-        //after coloring the vertex push into the queue
+        color[vertex] = 1; // mark it visited for the first with anything 0 or 1
+        // after coloring the vertex push into the queue
         q.push(vertex);
 
         while (!q.empty())
@@ -48,12 +52,14 @@ public:
             {
                 if (color[v] == -1)
                 {
-                    //color with an alternate color and then push it to the queue
+                    // color with an alternate color and then push it to the queue
                     color[v] = 1 - color[u];
                     q.push(v);
                 }
-                /*once again we are checking that if u and v are of same 
-                color return false*/
+                /*
+                once again we are checking that if u and v are of same
+                color return false
+                */
                 else if (color[v] == color[u])
                 {
                     return false;
@@ -64,11 +70,11 @@ public:
     }
 };
 
-//Using DFS
+// using DFS
 class Solution
 {
 public:
-    //Time: O(V+E), Space: O(V)
+    // Time: O(N+E), Space: O(N+E) + O(N) + O(N)
     bool isBipartite(vector<vector<int> > &graph)
     {
         int n = graph.size();
@@ -76,32 +82,36 @@ public:
         //-1 means no vertices are colored
         vector<int> color(n, -1);
 
-        /*this loop is for all vertices because it is given that graph might 
-        be disconnected, so we have to check for every vertices*/
+        /*
+        this loop is for all vertices because it is given that graph might
+        be disconnected, so we have to check for every vertices
+        */
         for (int i = 0; i < n; i++)
         {
             if (color[i] == -1)
-                if (!graphColoring(graph, i, color))
+                if (!checkBipartite(graph, i, color))
                     return false;
         }
         return true;
     }
 
-    bool graphColoring(vector<vector<int> > &graph, int vertex, vector<int> &color)
+    bool checkBipartite(vector<vector<int> > &graph, int vertex, vector<int> &color)
     {
-        //if the vertex is not colored color it
+        // if it is for the first time
         if (color[vertex] == -1)
             color[vertex] = 1;
 
-        //now, color it's adjacent vertices
+        // color the adjacent nodes
         for (auto it : graph[vertex])
         {
             if (color[it] == -1)
             {
+                // color with opposite color to its adjacent node
                 color[it] = 1 - color[vertex];
-                if (!graphColoring(graph, it, color))
+                if (!checkBipartite(graph, it, color))
                     return false;
             }
+            // if 'it' and 'vertex' are of same color then graph is not bipartite
             else if (color[it] == color[vertex])
                 return false;
         }
