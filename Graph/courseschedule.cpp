ae7d11e2128e728b70e98b408aa5f49ce9616b9b
Nitish Kumar <nitishkumargenius@gmail.com>
2022-02-25 13:25:23 +0530

    Updated Course Schedule

--- Graph/courseschedule.cpp
+++ Graph/courseschedule.cpp
@@ -3,87 +3,113 @@ Leetcode Question 207. Course Schedule
 https://leetcode.com/problems/course-schedule/
 */
 
+/*
+This question is all about detecting cycle in a graph or not. If cycle exists, then
+all the courses will be interdependent and we will not be able finish the courses.
+*/
 // Solution 1
 class Solution
 {
 public:
-    /*the basic idea of this question is we have to detect cycle in the graph.
-    Please read the standard problem of detecting the cycle in a graph and
-    you are good to go in this question. Time: O(V + E), Space: O(V)*/
-    bool canFinish(int numCourses, vector<vector<int> > &prerequisites)
+    // Time: O(V + E), Space: O(V) + O(V)
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
     {
-        vector<vector<int> > adj(numCourses, vector<int>());
-        for (vector<int> &pre : prerequisites)
-        {
-            adj[pre[0]].push_back(pre[1]);
-        }
-
-        vector<int> visited(numCourses, 0);
-
-        for (int i = 0; i < numCourses; i++)
-            if (visited[i] == 0 && !isCyclic(adj, visited, i))
-                return false;
-
-        return true;
-    }
-
-    bool isCyclic(vector<vector<int> > &adj, vector<int> &visited, int v)
-    {
-        if (visited[v] == 1)
-            return false;
-        if (visited[v] == 2)
-            return true;
-        visited[v] = 1;
-        for (int ad : adj[v])
-        {
-            if (!isCyclic(adj, visited, ad))
-                return false;
-        }
-
-        visited[v] = 2;
-        return true;
-    }
-};
-
-// Solution 2
-class Solution
-{
-public:
-    /*This is the slight variation of the above solution in terms of writing (not logic)
-    Time: O(V + E), Space: O(V)*/
-    bool canFinish(int numCourses, vector<vector<int> > &prerequisites)
-    {
+        // build the graph
         vector<int> adj[numCourses];
         for (auto pre : prerequisites)
         {
             adj[pre[0]].push_back(pre[1]);
         }
 
-        vector<int> visited(numCourses, 0);
+        vector<int> vis(numCourses, 0);
+        vector<int> dfsVis(numCourses, 0);
 
         for (int i = 0; i < numCourses; i++)
-            if (visited[i] == 0)
-                if (isCyclic(adj, visited, i))
+            if (vis[i] == 0)
+                if (isCyclic(adj, vis, dfsVis, i))
                     return false;
 
         return true;
     }
 
-    bool isCyclic(vector<int> adj[], vector<int> &visited, int v)
+    bool isCyclic(vector<int> adj[], vector<int> &vis, vector<int> &dfsVis, int v)
     {
-        if (visited[v] == 1)
-            return true; // cycle exists
 
-        if (visited[v] == 0)
+        vis[v] = 1;
+        dfsVis[v] = 1;
+
+        for (auto it : adj[v])
         {
-            visited[v] = 1; // mark it visited
-            for (int ad : adj[v])
+            if (!vis[it])
             {
-                if (isCyclic(adj, visited, ad))
+                if (isCyclic(adj, vis, dfsVis, it)) // if cycle exists return true
                     return true;
             }
+            else if (dfsVis[it] == 1)
+                return true; // if cycle exists return true
         }
-        visited[v] = 2; // this happens when we have visited the adjacent nodes of v
-        return false;   // if cycle doesn't exists
+
+        dfsVis[v] = 0;
+        return false;
+    }
+};
+
+/*
+We can also use kahn's Alogrithm to detect cycle in a graph. Because, if cycle exists
+ordering of the courses will not be possible, so topological sort will also not
+possible.
+*/
+// Solution 2
+class Solution
+{
+public:
+    // Time: O(V + E), Space: O(V) + O(V)
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        // build the graph
+        vector<int> adj[numCourses];
+        for (auto pre : prerequisites)
+        {
+            adj[pre[0]].push_back(pre[1]);
+        }
+
+        vector<int> indegree(numCourses, 0);
+
+        // calculate the indegrees of all nodes
+        for (int i = 0; i < numCourses; i++)
+        {
+            for (auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        // push all the nodes which have indegrees 0
+        for (int i = 0; i < indegree.size(); i++)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        // now start BFS or kahn's algorithm
+        int count = 0;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            count++;
+
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        if (count == numCourses)
+            return true; // cycle doesn't exists
+        return false;    // cycle exists
     }
 };
