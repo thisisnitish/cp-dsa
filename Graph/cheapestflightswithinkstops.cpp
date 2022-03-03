826f57f800790e55b2ac98a6e682ba1553c21f5e
Nitish Kumar <nitishkumargenius@gmail.com>
2022-03-03 12:32:38 +0530

    Updated Cheapest Flights Within K Stops

--- Graph/cheapestflightswithinkstops.cpp
+++ Graph/cheapestflightswithinkstops.cpp
@@ -3,51 +3,97 @@ Leetcode Question 787. Cheapest Flights Within K Stops
 https://leetcode.com/problems/cheapest-flights-within-k-stops/
 */
 
+// This will give TLE
 class Solution
 {
 public:
-    /*the basic idea is to implement the dijkstra's algorithm with some changes.
-    here we have to find the shortest path between src to dest with K+1 edges.
-    Time: O(E*logV)*/
     int findCheapestPrice(int n, vector<vector<int> > &flights, int src, int dst, int K)
     {
-        vector<vector<pair<int, int> > > adj(n);
+        vector<pair<int, int> > adj[n];
 
-        //creating the adjacency list
         for (vector<int> &flight : flights)
         {
-            //u => v, w
+            // u => (v, w)
             adj[flight[0]].push_back({flight[1], flight[2]});
         }
 
-        //creating the priority queue to store the {distance, node, number of edges}
         priority_queue<vector<int>, vector<vector<int> >, greater<vector<int> > > pq;
-        pq.push({0, src, K + 1});
+        pq.push({0, src, K + 1}); //(price, node, edges)
 
         while (!pq.empty())
         {
             vector<int> top = pq.top();
             pq.pop();
 
-            int d = top[0]; //distance
-            int u = top[1]; //node
-            int e = top[2]; //edges
+            int price = top[0];
+            int node = top[1];
+            int edges = top[2]; // no of edges we have from node 'u' to destination
 
-            //if the dst is same is a u then we have reached the destination
-            //then return it
-            if (u == dst)
-                return d;
-            //if not reach the destination then push into the queue and
-            //simultaneously decrease e values
-            if (e > 0)
+            // if we reach the destination then return the price
+            if (node == dst)
+                return price;
+            if (edges > 0)
             {
-                for (pair<int, int> &v : adj[u])
+                for (auto it : adj[node])
                 {
-                    pq.push({d + v.second, v.first, e - 1});
+                    int nextNode = it.first;
+                    int cost = it.second;
+                    pq.push({price + cost, nextNode, edges - 1});
                 }
             }
         }
-        //if we got nothing then return -1
+        return -1;
+    }
+};
+
+// This will passed the test cases
+class Solution
+{
+public:
+    // Time: O(NlogN)
+    int findCheapestPrice(int n, vector<vector<int> > &flights, int src, int dst, int K)
+    {
+        vector<pair<int, int> > adj[n];
+
+        for (vector<int> &flight : flights)
+        {
+            // u => (v, w)
+            adj[flight[0]].push_back({flight[1], flight[2]});
+        }
+
+        vector<int> dist(n, INT_MAX); // to avoid TLE
+        priority_queue<vector<int>, vector<vector<int> >, greater<vector<int> > > pq;
+        pq.push({0, src, 0}); //(price, node, edges)
+
+        while (!pq.empty())
+        {
+            vector<int> top = pq.top();
+            pq.pop();
+
+            int price = top[0];
+            int node = top[1];
+            int edges = top[2];
+
+            // if we reach the destination then return the price
+            if (node == dst)
+                return price;
+
+            if (edges > K)
+                continue;
+
+            // optimization
+            if (dist[node] < edges)
+                continue;
+            dist[node] = edges;
+
+            for (auto it : adj[node])
+            {
+                int nextNode = it.first;
+                int cost = it.second;
+                // increase the edge by 1 in order to reach the next step
+                pq.push({price + cost, nextNode, edges + 1});
+            }
+        }
         return -1;
     }
 };
