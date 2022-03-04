62781a4116da372e222ff248c5b210d1d90b4b90
Nitish Kumar <nitishkumargenius@gmail.com>
2022-03-04 12:13:09 +0530

    Updated All Paths From Source to Target

--- Graph/allpathfromsourcetotarget.cpp
+++ Graph/allpathfromsourcetotarget.cpp
@@ -6,31 +6,35 @@ https://leetcode.com/problems/all-paths-from-source-to-target/
 class Solution
 {
 public:
-    /*the basic idea is to just implement the dfs with some tweaks.
-    Time: O(V+E)*/
-    int size;
+    // Time: O(2^n), Space: O(n)
     vector<vector<int> > result;
-    vector<int> path;
     vector<vector<int> > allPathsSourceTarget(vector<vector<int> > &graph)
     {
         int source = 0;
-        size = graph.size() - 1;
-        dfs(graph, source);
+        int size = graph.size() - 1;
+        vector<int> path;
+        dfs(graph, source, size, path);
         return result;
     }
 
-    void dfs(vector<vector<int> > &graph, int source)
+    void dfs(vector<vector<int> > &graph, int source, int size, vector<int> &path)
     {
+
+        // add the node to the path
         path.push_back(source);
 
-        /*if this happens then there is not possible to move further from 
-        that path, so we have to start again from beginning for other path*/
+        // base case
         if (source == size)
+        {
             result.push_back(path);
+            return;
+        }
 
         for (auto v : graph[source])
         {
-            dfs(graph, v);
+            dfs(graph, v, size, path);
+            /*pop here because you need to start from another
+            edge in order to reach target*/
             path.pop_back();
         }
     }
