dc4c967c50363493d87374de2caa5d4a6a48d1ee
Nitish Kumar <nitishkumargenius@gmail.com>
2022-03-08 13:23:11 +0530

    Updated Keys and Rooms

--- Graph/keysandrooms.cpp
+++ Graph/keysandrooms.cpp
@@ -3,21 +3,29 @@ Leetcode Question 841. Keys and Rooms
 https://leetcode.com/problems/keys-and-rooms/
 */
 
+/*
+The crux of the problem is, it asks us to visit all the rooms or we can say visit
+all the nodes. But there is catch here, you can't visit all the rooms because all
+rooms are closed except 0.
+
+So, you can visit 0 and then get the keys from there for the next rooms and unlock
+them. Or, we can say, get the adjacent nodes and visit them one by one.
+
+We can use DFS traversal for this because this is a simple DFS traversal problem.
+Start from the src node 0 and take dfs call and visit the graph and while visiting
+mark the node visited. If every node is visited return true else return false.
+*/
 class Solution
 {
 public:
-    /*the basic idea is, since the question is asking visit the every room,
-    so definately we have to use traversal, so, we are using dfs.
-    Time: O(N+E), Space: O(N)*/
+    // Time: O(V+E), Space: O(V+E)
     bool canVisitAllRooms(vector<vector<int> > &rooms)
     {
         int n = rooms.size();
         vector<bool> visited(n, false);
         int source = 0;
-        //since we have to start dfs from the source node only
         dfs(source, rooms, visited);
 
-        //checking that every node is visited or not
         for (int i = 0; i < visited.size(); i++)
         {
             if (!visited[i])
