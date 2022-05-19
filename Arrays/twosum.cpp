bdbeeb71de79be09ac717ba2299aa1c29f3ad790
Nitish Kumar <nitishkumargenius@gmail.com>
2022-05-19 07:42:04 +0530

    Updated Two Sum

--- Arrays/twosum.cpp
+++ Arrays/twosum.cpp
@@ -4,61 +4,66 @@ https://leetcode.com/problems/two-sum/
 */
 
 /*
-The Brute Force will take O(n^2) as we have to traverse the array and check for every 
-possible combination and then choose the index.
+Brute Force will be O(n^2), but then we cannot do it as it will not be an efficient
+algorithm to use.
+Further optimization I decided was O(nlogn). In this case, we can reduce the second
+loop from n to logn making the entire time complexity O(nlogn).
+But still it is not enough, We have to make it to O(n). To do so, we can use hashing
+for this and store the index of each element.
+While traversing the array, we can check for the complement in the hash-map and
+check whether the index of complement and index of nums[i] is different or not.
+
+This is a really good problem to start algorithm skills. I've done brute force. I
+believe, It is self understood. But, In an interview, we have to start from the
+Brute Force no matter how well you know the question.
 */
-//Two pass
+
+// Two Pass
 class Solution
 {
 public:
-    //Time: O(n), Space: O(n)
+    // Time: O(N), Space: O(N)
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]] = i;
+        }
 
-        unordered_map<int, int> m;
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
-            m[nums[i]] = i;
-
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < nums.size(); i++)
         {
             int complement = target - nums[i];
-            if (m.find(complement) != m.end())
+
+            if (mp.find(complement) != mp.end())
             {
-                if (m[complement] != i)
-                {
-                    ans.push_back(i);
-                    ans.push_back(m[complement]);
-                    break;
-                }
+                if (mp[complement] != i)
+                    return {i, mp[complement]};
             }
         }
-        return ans;
+        return {};
     }
 };
 
-//Single pass
+// Single Pass
 class Solution
 {
 public:
-    //Time: O(n), Space: O(n)
+    // Time: O(N), Space: O(N)
     vector<int> twoSum(vector<int> &nums, int target)
     {
         unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++)
         {
             int complement = target - nums[i];
+
             if (mp.find(complement) != mp.end())
             {
                 if (mp[complement] != i)
-                    return {mp[complement], i};
+                    return {i, mp[complement]};
             }
             else
-            {
                 mp[nums[i]] = i;
-            }
         }
         return {};
     }
