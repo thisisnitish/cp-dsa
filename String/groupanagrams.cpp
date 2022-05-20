4badd410167e720513bfc1e1a610db465227f699
Nitish Kumar <nitishkumargenius@gmail.com>
2022-05-20 06:18:27 +0530

    Updated Group Anagrams

--- String/groupanagrams.cpp
+++ String/groupanagrams.cpp
@@ -3,24 +3,39 @@ Leetcode Question 49. Group Anagrams
 https://leetcode.com/problems/group-anagrams/
 */
 
+/*
+It was very simple implementation of hashing. Now, new concepts I learned in this
+question.
+I was able to do it in 7 mins. 59 sec.
+*/
+
 class Solution
 {
 public:
+    // Time: O(n * m.logm + n), Space: O(n)
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        vector<vector<string>> result;
-        unordered_map<string, vector<string>> mp;
-
-        for (auto s : strs)
+        // base case
+        if (strs.size() == 1)
         {
-            string c = s;
-            sort(s.begin(), s.end());
-            mp[s].push_back(c);
+            if (strs[0] == "")
+                return {{""}};
+            else
+                return {{strs[0]}};
         }
 
-        for (auto ele : mp)
+        unordered_map<string, vector<string>> mp;
+        for (auto s : strs)
         {
-            result.push_back(ele.second);
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto &[k, v] : mp)
+        {
+            result.push_back(v);
         }
         return result;
     }
