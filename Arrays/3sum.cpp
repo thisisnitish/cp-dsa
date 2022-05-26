91b3e1009717338d3530e4e40ea8a8bb53f36a16
Nitish Kumar <nitishkumargenius@gmail.com>
2022-05-26 13:13:40 +0530

    Updated 3Sum

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -3,14 +3,19 @@ Leetcode Question 15. 3Sum
 https://leetcode.com/problems/3sum/
 */
 
+// I was able to solve this problem in 18 mins.
+
 class Solution
 {
 public:
-    //Time: O(nlogn + n^2), Space: O(n)
-    vector<vector<int> > threeSum(vector<int> &nums)
+    // Time: O(NlogN + N^2), Space: O(N)
+    vector<vector<int>> threeSum(vector<int> &nums)
     {
 
-        vector<vector<int> > threesum;
+        if (nums.size() == 0)
+            return {};
+
+        vector<vector<int>> threesum;
         int len = nums.size();
 
         sort(nums.begin(), nums.end());
@@ -18,7 +23,7 @@ public:
         for (int i = 0; i < len; i++)
         {
 
-            //to handle the duplicates
+            // to handle the duplicates initially
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
@@ -32,11 +37,11 @@ public:
                 {
                     threesum.push_back({x, nums[left], nums[right]});
 
-                    //to handle the duplicates for left pointer
+                    // to handle the duplicates for left pointer (for two sum)
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
 
-                    //to handle the duplicates for right pointer
+                    // to handle the duplicates for right pointer (for two sum)
                     while (left < right && nums[right] == nums[right - 1])
                         right--;
 
