f5f89c760eec5bf581056e06aeadbc3fa0f99bf5
Nitish Kumar <thisisnitish30@gmail.com>
2024-01-06 12:58:34 +0530

    added sol 2 for question 153 in binary search

--- BinarySearch/findminimuminrotatedsortedarray.cpp
+++ BinarySearch/findminimuminrotatedsortedarray.cpp
@@ -3,21 +3,22 @@ Leetcode Question 153. Find Minimum in Rotated Sorted Array
 https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 */
 
+// Solution 1
 class Solution
 {
 public:
     /*the basic idea is to apply binary search algorithm,
     first check for if first element is greater than the last
     element then return the first element because array is already sorted
-    but if this is not the case then find the middle element and check if 
-    it is less than the right, if yes then move towards the left side of 
+    but if this is not the case then find the middle element and check if
+    it is less than the right, if yes then move towards the left side of
     array else move towards the right side of the array. Time: O(logN),
     Space: O(1)*/
     int findMin(vector<int> &nums)
     {
         int left = 0, right = nums.size() - 1;
 
-        //base case i.e. if the array is sorted already
+        // base case i.e. if the array is sorted already
         if (nums[left] < nums[right])
             return nums[left];
 
@@ -34,5 +35,41 @@ public:
             }
         }
         return nums[left];
+    }
+};
+
+// Solution 2
+class Solution
+{
+public:
+    // Time: O(logN), Space: O(1)
+    int findMin(vector<int> &nums)
+    {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+
+        if (nums[low] < nums[high])
+            return nums[low];
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            int next = (mid + 1) % n;
+            int prev = (mid + n - 1) % n;
+
+            // minimum element will always be lesser than either side of element
+            if (nums[prev] >= nums[mid] && nums[mid] <= nums[next])
+                return nums[mid];
+
+            // move to unsorted half because it only contains
+            // the minimum element
+            if (nums[0] <= nums[mid])
+                low = mid + 1;
+            else if (nums[mid] <= nums[n - 1])
+                high = mid - 1;
+        }
+
+        return -1;
     }
 };
